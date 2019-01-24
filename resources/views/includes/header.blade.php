62862ee24fda80748a926983b2a873cb7619ced2
brieuc <brieucmailpro@gmail.com>
2019-01-25 00:43:53 +0100

    🍉

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="text-center">
+                    <div class="row">
+                        <div class="col">
+                            <h1>
+                                <a href="{{ url('/')  }}">Snapmail</a>
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
